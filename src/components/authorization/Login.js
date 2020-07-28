97863899c9f228ba916b7a0612572cc9bcdaec49
Matthew Sedlacek <sedlacek1991@gmail.com>
2020-07-28 08:36:17 -0700

    masks password input for login

--- src/components/authorization/Login.js
+++ src/components/authorization/Login.js
@@ -1,18 +1,10 @@
 import React from "react";
 import { api } from "../../services/api";
 import { Link } from "react-router-dom";
-import Avatar from "@material-ui/core/Avatar";
 import Button from "@material-ui/core/Button";
-import CssBaseline from "@material-ui/core/CssBaseline";
 import TextField from "@material-ui/core/TextField";
-import FormControlLabel from "@material-ui/core/FormControlLabel";
-import Checkbox from "@material-ui/core/Checkbox";
-// import Link from "@material-ui/core/Link";
-import Grid from "@material-ui/core/Grid";
 import Box from "@material-ui/core/Box";
-// import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
 import Typography from "@material-ui/core/Typography";
-import { withStyles } from "@material-ui/core/styles";
 import Container from "@material-ui/core/Container";
 
 class Login extends React.Component {
@@ -96,7 +88,8 @@ class Login extends React.Component {
                 id="password"
                 label="Password"
                 name="password"
-                autoComplete="password"
+                type="password"
+                autoComplete="current-password"
                 autoFocus
                 value={fields.password}
                 onChange={this.handleChange}
@@ -134,6 +127,9 @@ class Login extends React.Component {
             </Link>
           </div>
         </div>
+        <Box mt={8}>
+          <div>{this.Copyright()}</div>
+        </Box>
       </Container>
     );
   }
