ea69ea988ae61e6f69f504d04ab1cb166f4b3dd6
Matthew Sedlacek <sedlacek1991@gmail.com>
2020-07-28 10:58:43 -0700

    adds news formatting

--- src/components/profile/NewsList.js
+++ src/components/profile/NewsList.js
@@ -10,45 +10,11 @@ const NewsList = (props) => {
 
   return (
     <Fragment>
-      {/* <div>
-        <h1>{props.portfolio.name}</h1>
+      <div className="profileContainer">
+        <table>
+          <tbody>{props.news && renderNews()}</tbody>
+        </table>
       </div>
-      <div>
-        <h2>{props.portfolio.value}</h2>
-      </div> */}
-
-      <table>
-        <tbody>
-          {/* <tr>
-            <th>
-              <h3>Ticker</h3>
-            </th>
-            <th>
-              <h3>Name</h3>
-            </th>
-            <th>
-              <h3>Quantity</h3>
-            </th>
-            <th>
-              <h3>Purchase Price</h3>
-            </th>
-            <th>
-              <h3>Current Price</h3>
-            </th>
-            <th>
-              <h3>Total Value</h3>
-            </th>
-            <th>
-              <h3>Today's Change</h3>
-            </th>
-            <th>
-              <h3>Transaction Type</h3>
-            </th>
-          </tr> */}
-          {props.news && renderNews()}
-        </tbody>
-      </table>
-      <div></div>
     </Fragment>
   );
 };
